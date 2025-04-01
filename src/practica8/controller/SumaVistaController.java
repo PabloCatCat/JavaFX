f089cb7f4ace6c3cfeb8803d444e327cebaf645d
Pablo <pabherala2@alu.edu.gva.es>
2025-04-01 23:40:27 +0200

    commit casa

--- src/practica8/controller/SumaVistaController.java
+++ src/practica8/controller/SumaVistaController.java
@@ -2,10 +2,15 @@ package practica8.controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 
-public class SumaVistaController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class SumaVistaController implements Initializable {
 
     @FXML
     private Button btnSumar;
@@ -21,8 +26,25 @@ public class SumaVistaController {
 
     @FXML
     void btnSumarClick(ActionEvent event) {
-        int resultado = Integer.parseInt(txtNum1.getText()) + Integer.parseInt(txtNum2.getText());
-        txtResult.setText(Integer.toString(resultado));
+        double resultado;
+
+        try {
+            resultado = Double.parseDouble(txtNum1.getText()) + Double.parseDouble(txtNum2.getText());
+            txtResult.setText(Double.toString(resultado));
+        } catch (NumberFormatException e) {
+            mostrarAlertaError("Error al hacer la suma", "Los operandos deben ser números");
+        }
     }
 
+    private void mostrarAlertaError(String header, String content) {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setHeaderText(header);
+        alert.setContentText(content);
+        alert.show();
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        btnSumar.setOnAction(this::btnSumarClick);
+    }
 }
